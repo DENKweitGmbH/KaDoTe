04ad8166ec82ce6481de2a1385ba7b2c409fe9a1
Georg <georg.keller@denkweit.de>
2025-02-19 16:16:43 +0100

    Bugfix

--- client.py
+++ client.py
@@ -605,10 +605,10 @@ class Wenglor:
         self.harvester.update()
         self.log.debug("GigE devices: %s", self.harvester.device_info_list)
         self.ia = self.harvester.create()
-        self.ia.remove_device.node_map.AcquisitionMode = "SingleFrame"
-        self.ia.remove_device.node_map.TriggerSelector = "FrameStart"
-        self.ia.remove_device.node_map.TriggerMode = "On"
-        self.ia.remove_device.node_map.TriggerSource = "Software"
+        self.ia.remote_device.node_map.AcquisitionMode = "SingleFrame"
+        self.ia.remote_device.node_map.TriggerSelector = "FrameStart"
+        self.ia.remote_device.node_map.TriggerMode = "On"
+        self.ia.remote_device.node_map.TriggerSource = "Software"
         self.point_cloud_data: Buffer | None = None
 
     def _start_server(self, timeout: float = 5.0) -> None:
@@ -649,7 +649,7 @@ class Wenglor:
         self.log.info("Acquiring point cloud...")
         try:
             self.ia.start()
-            self.ia.remove_device.node_map.TriggerSoftware.execute()
+            self.ia.remote_device.node_map.TriggerSoftware.execute()
             if self.point_cloud_data is not None:
                 self.point_cloud_data.queue()
             self.point_cloud_data = self.ia.fetch(timeout=20)
